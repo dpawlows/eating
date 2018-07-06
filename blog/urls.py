90fb70dfbd7e3440052aa8d12b162dec538bb6db
Dave Pawlowski <dpawlows80@gmail.com>
2018-07-06 08:25:11 -0500

    added view and template for detal page

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('',views.post_list,name="post_list"),
-
+    path('post/<int:pk>/',views.post_detail,name='post_detail'),
 ]
